c578608429ee3295ae79df03b4d35a6f42652011
Henrique Mendonça Castelar Campos <henriquemendonacastelar@gmail.com>
2024-10-28 18:09:50 -0300

    Adicionando a obtenção do número de processos e do tempo de simulação do usuário

--- app/src/main/kotlin/io/github/henriquemcc/simulacao/exclusao/mutua/distribuida/lamport/Lamport.kt
+++ app/src/main/kotlin/io/github/henriquemcc/simulacao/exclusao/mutua/distribuida/lamport/Lamport.kt
@@ -1,10 +1,25 @@
 package io.github.henriquemcc.simulacao.exclusao.mutua.distribuida.lamport
 
+import io.github.henriquemcc.simulacao.exclusao.mutua.distribuida.myio.readDouble
+import io.github.henriquemcc.simulacao.exclusao.mutua.distribuida.myio.readInteger
+import io.github.henriquemcc.simulacao.exclusao.mutua.distribuida.myio.readString
+import kotlin.math.floor
+
 fun main() {
-    val algoritmoLamport = AlgoritmoLamport(20)
+
+    // Obtendo quantidade de processos
+    val numeroProcessos = readInteger("Número de processos:", IntRange(1, Int.MAX_VALUE))
+
+    // Obtendo o tempo de simulação
+    val tempoSimulacao = readDouble("Tempo de simulação (em segundos):", LongRange(1, Double.MAX_VALUE.toLong()))
+
+    // Criando uma instância do algoritmo de Lamport
+    val algoritmoLamport = AlgoritmoLamport(numeroProcessos)
+
+    // Iniciando a simulação
     algoritmoLamport.start()
 
-    // Finalizando o programa após ele ter sido executado por 1 minuto
-    Thread.sleep(60000)
+    // Finalizando o programa após ele ter sido executado pelo tempo especificado
+    Thread.sleep(floor(tempoSimulacao * 1000).toLong())
     algoritmoLamport.stopFlag.set(true)
 }
